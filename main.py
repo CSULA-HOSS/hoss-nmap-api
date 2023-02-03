56e7aa7a2a1838c53e36c2c27b5d7be6d6446949
Luis Cortez <luiscortez@Luiss-Air.attlocal.net>
2023-02-03 10:26:24 -0800

    HOSS-API: NMAP detailed OS scanner

---
+++ main.py
@@ -0,0 +1,21 @@
+import nmap
+import uvicorn
+from fastapi import FastAPI, Request
+
+
+app = FastAPI()
+
+    
+@app.get("/")
+def read_root():
+    return {"HOSS": "Home & Office Security Scanner"}
+
+@app.get("/scan")
+def scan(request: Request):
+    client_host = request.client.host
+
+    nm = nmap.PortScanner() 
+    return nm.scan(hosts=f'{client_host}/24', arguments='-sS -O')
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000)
