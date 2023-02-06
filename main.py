df412898219968ac1432cbce3560e388fca58176
Luis Cortez <luiscortez@Luiss-MacBook-Air.local>
2023-02-06 12:34:14 -0800

    HOSS-API: Test API to be deployed to AWS EC2 instance

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ import nmap
 import uvicorn
 from fastapi import FastAPI, Request
 
-
 app = FastAPI()
 
-    
 @app.get("/")
 def read_root():
     return {"HOSS": "Home & Office Security Scanner"}
@@ -17,5 +15,9 @@ def scan(request: Request):
     nm = nmap.PortScanner() 
     return nm.scan(hosts=f'{client_host}/24', arguments='-sS -O')
 
-if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+@app.get("/test")
+def test(fName, lName):
+    return f'Hello {fName} {lName}, this is a test endpoint'
+    
+# if __name__ == '__main__':
+#     uvicorn.run(app, host='0.0.0.0', port=8000)
